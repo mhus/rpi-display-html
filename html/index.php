9d2289d251b8d579c315c9bdf279257900c47336
Mike Hummel <mh@mhus.de>
2023-01-11 10:59:29 +0100

    add refresh link

--- html/index.php
+++ html/index.php
@@ -3,8 +3,13 @@
     <meta http-equiv="refresh" content="10">
   </header>
 <body>
-<?php
-echo "<h1>" . date("h:i") . "</h1>";
+<center>
+<a href="#">Refresh</a>    
+<font size=40>
+<?php 
+date_default_timezone_set("Europe/Paris");
+echo date("h:i"); 
 ?>
+</font></center>
 </body>
 </html>
